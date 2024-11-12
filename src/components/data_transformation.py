3f8303c4f8dbf91fc85ce224b5c25580dc618988
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-11-13 03:21:30 +0600

    created pipeline for datatransforming

---
+++ src/components/data_transformation.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd 
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_file_path = 'artifacts\preprocessor.pkl'
+
+class TransformationPipeline:
+    def __init__(self):
+        self.data_transformatio_config = DataTransformationConfig()
+
+    def DataTransformerPipeline(self):
+        try:
+            numerical_columns = ["writing_score", "reading_score"]
+            categorical_columns = [
+                "gender",
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preparation_course",
+            ]
+
+            num_pipeline= Pipeline(
+                steps=[
+                ("imputer",SimpleImputer(strategy="median")),
+                ("scaler",StandardScaler())
+
+                ]
+            )
+
+            category_pipeline=Pipeline(
+                steps=[
+                ("imputer",SimpleImputer(strategy="most_frequent")),
+                ("one_hot_encoder",OneHotEncoder()),
+                ("scaler",StandardScaler(with_mean=False))
+                ]
+
+            )
+
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
+
+            preprocessor = ColumnTransformer(
+                [
+                    ("num_pipeline", num_pipeline, numerical_columns)
+                    ("category_pipeline", category_pipeline, categorical_columns)
+                ]
+            )
+            return preprocessor
+
+        
+
+
+
+        except Exception as e:
+            raise CustomException(e)
+
+
+
