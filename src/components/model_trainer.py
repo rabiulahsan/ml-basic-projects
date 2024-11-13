9f3bbedea8241835af987ebb7ce03920e414d693
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-11-13 18:52:15 +0600

    create model training config and import data

---
+++ src/components/model_trainer.py
@@ -0,0 +1,48 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor 
+from sklearn.ensemble import AdaBoostRegressor, GradientBoostingRegressor, RandomForestRegressor
+from catboost import CatBoostRegressor
+from xgboost import XGBRegressor
+
+from src.logger import logging
+from src.exception import CustomException
+from src.utils import save_object
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_path:str = 'artifacts\model.pkl'
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    
+    def initiate_model_trainer(self, train_arr, test_arr, preprocessor_path):
+        try:
+            logging.info("split trainig and testing input data.")
+
+            X_train,y_train,X_test,y_test=(
+                train_arr[:,:-1],
+                train_arr[:,-1],
+                test_arr[:,:-1],
+                test_arr[:,-1]
+            )
+
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+
+        except Exception as e:
+            raise CustomException(e)
